1bb60c8932bdf068a4af22a7eb3fc90271163ebf
Brock Porzio <brockporzio@Brocks-MacBook-Pro.local>
2024-06-07 16:27:10 -0600

    feature/calendar setting callendar to 5 days

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,23 +1,28 @@
 import React from 'react';
 
-const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const daysInMonth = Array.from({ length: 28 }, (_, i) => i + 1); // Example for a 30-day month
+const daysOfWeek = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+const hoursInDay = Array.from({ length: 8 }, (_, i) => i + 1);
 
 const Calendar = () => {
   return (
     <div className="container border border-gray-800 max-w-lg mx-auto p-4">
-      <div className="grid grid-cols-7 gap-1 text-center font-bold">
+      <div className="grid grid-cols-5 gap-1 text-center font-bold">
         {daysOfWeek.map((day) => (
           <div key={day} className="p-2 border border-gray-300 bg-gray-100">
             {day}
           </div>
         ))}
       </div>
-      <div className="grid grid-cols-7 gap-1 text-center mt-2">
-        {daysInMonth.map((day) => (
-          <div key={day} className="p-4 border border-gray-300">
-            {day}
-          </div>
+      <div className="grid grid-cols-5 gap-0.5 text-center mt-2">
+        {daysOfWeek.map((day, dayIndex) => (
+          hoursInDay.map((hour) => (
+            <div 
+              key={`${dayIndex}-${hour}`} 
+              id={`hour-${hour}-day-${dayIndex}`} 
+              className="p-4 border border-gray-300">
+              {/* Content can be added here if needed */}
+            </div>
+          ))
         ))}
       </div>
     </div>
